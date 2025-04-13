6125c1c642ff34273af37744e015a77d740ba478
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2025-04-13 18:28:51 -0300

    Update ufs.module.ts

--- src/ufs/ufs.module.ts
+++ src/ufs/ufs.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { UfsService } from './ufs.service';
-import { UfsController } from './ufs.controller';
+// ufs.module.ts
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Uf } from "./entities/uf.entity"; // Ajuste o caminho conforme sua estrutura
+import { UfsService } from "./ufs.service";
+import { UfsController } from "./ufs.controller";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Uf])], // Isso torna UfRepository disponível
   controllers: [UfsController],
   providers: [UfsService],
 })
